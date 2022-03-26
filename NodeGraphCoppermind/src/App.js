823e11936fc5832143e7b580e7df1527feb26532
Malthemester <malthemester@gmail.com>
2022-03-26 17:30:30 +0100

    canvas

--- NodeGraphCoppermind/src/App.js
+++ NodeGraphCoppermind/src/App.js
@@ -1,13 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from "react";
+
 import './App.css';
 
 function App() {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+
+  const [zoom, setZoom] = useState(1);
+
+
+  useEffect (() => {
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth * 2
+    canvas.height = window.innerHeight * 2
+    canvas.style.width = `${window.innerWidth}px`
+    canvas.style.height = `${window.innerHeight}px`
+
+    const ctx = canvas.getContext("2d")
+    ctx.scale(zoom,zoom)
+    contextRef.current = ctx
+    draw()
+  }, [])
+
+
+  useEffect (() => {
+    contextRef.current.scale(zoom,zoom)
+    draw()
+  }, [zoom])
+
+
+  const draw = () => {
+    console.log(canvasRef.current)
+    // contextRef.current.fillStyle = "black"
+    
+    // contextRef.current.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height)
+    
+    contextRef.current.beginPath()
+    contextRef.current.fillStyle = "green"
+    contextRef.current.arc(500, 500, 10, 0, Math.PI * 2)
+    contextRef.current.fill()
+
+  }
+
   return (
-    <div className="App">
-
-      <canvas id="canvas1"></canvas>
-
-    </div>
+    <canvas
+      ref={canvasRef}
+      onWheel ={(e) => {
+        console.log(e.deltaY > 0)
+        setZoom(e.deltaY > 0 ? 1.1 : .9)
+        console.log(zoom)
+      
+      }}
+    />
   );
 }
 
