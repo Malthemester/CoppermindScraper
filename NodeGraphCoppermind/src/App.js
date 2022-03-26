366f9144c4ecaaae3a31f0229305fc43f8b3dcfe
Malthemester <malthemester@gmail.com>
2022-03-26 18:38:13 +0100

    now useing react-force-graph

--- NodeGraphCoppermind/src/App.js
+++ NodeGraphCoppermind/src/App.js
@@ -1,57 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
+import { ForceGraph2D, ForceGraph3D, ForceGraphVR, ForceGraphAR } from 'react-force-graph';
 
 import './App.css';
 
 function App() {
 
-  const canvasRef = useRef(null)
-  const contextRef = useRef(null)
 
-  const [zoom, setZoom] = useState(1);
-
-
-  useEffect (() => {
-    const canvas = canvasRef.current
-    canvas.width = window.innerWidth * 2
-    canvas.height = window.innerHeight * 2
-    canvas.style.width = `${window.innerWidth}px`
-    canvas.style.height = `${window.innerHeight}px`
-
-    const ctx = canvas.getContext("2d")
-    ctx.scale(zoom,zoom)
-    contextRef.current = ctx
-    draw()
-  }, [])
-
-
-  useEffect (() => {
-    contextRef.current.scale(zoom,zoom)
-    draw()
-  }, [zoom])
-
-
-  const draw = () => {
-    console.log(canvasRef.current)
-    // contextRef.current.fillStyle = "black"
-    
-    // contextRef.current.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height)
-    
-    contextRef.current.beginPath()
-    contextRef.current.fillStyle = "green"
-    contextRef.current.arc(500, 500, 10, 0, Math.PI * 2)
-    contextRef.current.fill()
-
-  }
+  const myData= require('./myData.json'); 
+  console.log(myData)
 
   return (
-    <canvas
-      ref={canvasRef}
-      onWheel ={(e) => {
-        console.log(e.deltaY > 0)
-        setZoom(e.deltaY > 0 ? 1.1 : .9)
-        console.log(zoom)
-      
-      }}
+    <ForceGraph3D
+      graphData={myData}
     />
   );
 }
