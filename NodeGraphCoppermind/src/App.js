b2c97c3f0307f95f0e780d2308b20f36cbc8dc4c
Malthemester <malthemester@gmail.com>
2022-04-04 00:31:28 +0200

    Working on highlighting

--- NodeGraphCoppermind/src/App.js
+++ NodeGraphCoppermind/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useMemo } from "react";
+import React, { useEffect, useRef, useState, useMemo, useCallback } from "react";
 import { ForceGraph2D, ForceGraph3D, ForceGraphVR, ForceGraphAR } from 'react-force-graph';
 
 import './App.css';
@@ -7,48 +7,23 @@ function App() {
   const graphRef = useRef();
   const [hiddenTags, setHiddenTags] = useState(["Alcatraz", "The Rithmatist", "Reckoners", "Legion", "Skyward"]);
 
-
   useEffect(() => {
-
     graphRef.current.d3Force("link").distance(30);
-    graphRef.current.d3Force('charge').strength(-500);
-    // graphRef.current.d3Force('collide', d3.forceCollide(4));
-
-    // graphRef.current.d3Force('center', null);
-    // graphRef.current.d3Force('charge', null);
-
+    graphRef.current.d3Force('charge').strength(-600);
   }, []);
 
   const nodesData = require('./nodesData.json');
   const linksData = require('./linksData.json');
 
-
-  // console.log(nodesData.filter((node) => node.bookTags.some(tag => hiddenTags.includes(tag))))
-
   const graphData = useMemo(() => {
 
-    // var data = {
-    //   nodes: nodesData.filter((node) => !hiddenTags.includes(node.bookTags)),
-    //   links: linksData
-    // }.filter((node) => node.bookTags.some(tag => !hiddenTags.includes(tag)))
-
     var filterdNodes = nodesData.filter((node) => node.bookTags.every(tag => hiddenTags.includes(tag)))
-    console.log(filterdNodes)
-
     var filterdNodesIDs = filterdNodes.map(node => node.id)
-
-    console.log(filterdNodesIDs)
-    console.log("Here")
 
     const data = {
       nodes: nodesData.filter((node) => !filterdNodesIDs.includes(node.id)),
       links: linksData.filter((link) => !(filterdNodesIDs.includes(link.target) || filterdNodesIDs.includes(link.source)))
     };
-
-    console.log("Here2")
-
-    console.log(data.nodes)
-    console.log(data.links)
 
     return data
   }, [hiddenTags]);
@@ -57,14 +32,30 @@ function App() {
     window.open(node.url);
   }
 
+  const NODE_R = 4;
+  const paintRing = useCallback((node, ctx) => {
+    // add ring just for highlighted nodes
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, (4 * Math.pow((node.val), 0.5056)) + 4 , 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    console.log(node)
+    console.log(3.8763 * Math.pow((node.val), 0.5056))
+    console.log(3.8763 * 40)
+  }, []);
+
   return (
     <ForceGraph2D
       ref={graphRef}
+      nodeRelSize={NODE_R}
+      autoPauseRedraw={false}
       warmupTicks={100}
       cooldownTicks={0}
       graphData={graphData}
       enableNodeDrag={false}
       onNodeClick={handleClick}
+      nodeCanvasObjectMode={node => (((node.id == 'Nightblood' || node.id == 'Cosmere') || node.id == 'Cognitive Shadow')  || node.id == 'Tress of the Emerald Sea')? 'before' : undefined}
+      nodeCanvasObject={paintRing}
     />
   );
 }
