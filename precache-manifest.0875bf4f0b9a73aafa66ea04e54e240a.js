7bda4eb858246d395ba05ca3730602a3b434730f
Malthemester <malthemester@gmail.com>
2022-04-13 13:28:35 +0200

    Updates

---
+++ precache-manifest.0875bf4f0b9a73aafa66ea04e54e240a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f6b0472017e7c9a090f",
+    "url": "/static/css/main.b92387ce.chunk.css"
+  },
+  {
+    "revision": "5f6b0472017e7c9a090f",
+    "url": "/static/js/main.5f6b0472.chunk.js"
+  },
+  {
+    "revision": "8f8545716d4917a254e8",
+    "url": "/static/js/1.8f854571.chunk.js"
+  },
+  {
+    "revision": "d76eb8c72db78aa93feb",
+    "url": "/static/js/2.d76eb8c7.chunk.js"
+  },
+  {
+    "revision": "2642c268375d74d54d33",
+    "url": "/static/js/runtime~main.2642c268.js"
+  },
+  {
+    "revision": "8c9286f495ea042c055b3f49fb614c99",
+    "url": "/index.html"
+  }
+];
